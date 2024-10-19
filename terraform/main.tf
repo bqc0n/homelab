b2ec62a2f7c8803d5d69ecfea5b1885db4a0b955
bqc0n <b4c0nchan@gmail.com>
2024-10-20 00:37:05 +0900

    forgot

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,29 @@
 terraform {
   required_providers {
     proxmox = {
-      source = "Telmate/proxmox"
+      source  = "Telmate/proxmox"
       version = "3.0.1-rc4"
     }
   }
 }
 
-provider "proxmox" {}
+provider "proxmox" {
+  pm_api_url = "https://192.168.1.10:8006/api2/json"
+}
 
 locals {
   ssh_public_keys = <<-EOT
   ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE65lCWo/lvkIpk2NEnXuOdmruKsPOZyzgndg7y/0Kgr
   EOT
-
-  ubuntu_server_iso="local:iso/ubuntu-24.04.1-live-server-amd64.iso"
 }
 
-resource "proxmox_lxc" "core" {
+resource "proxmox_lxc" "dnsmasq" {
+  vmid         = 201
   target_node  = "pve02"
-  hostname     = "core"
+  hostname     = "dnsmasq"
   ostemplate   = "local:vztmpl/ubuntu-24.04-2_amd64.tar.zst"
   password     = "mou.mai"
-  start = true
+  start        = true
   unprivileged = true
 
   memory = 4096
@@ -33,51 +34,57 @@ resource "proxmox_lxc" "core" {
     size    = "16G"
   }
 
-  nameserver = "127.0.0.1"
-
-  ssh_public_keys = "${local.ssh_public_keys}"
+  ssh_public_keys = local.ssh_public_keys
 
   network {
     name   = "eth0"
     bridge = "vmbr0"
     ip     = "192.168.1.2/24"
-    gw = "192.168.1.1"
+    gw     = "192.168.1.1"
   }
 }
 
 resource "proxmox_vm_qemu" "docker_runner" {
-  target_node               = "pve02"
-  name                      = "docker-holder"
-  vm_state = "running"
+  vmid        = 202
+  target_node = "pve02"
+  name        = "docker-runner"
+  vm_state    = "running"
 
-  cores = 8
-  memory = 16384
-  os_type = "ubuntu"
+  clone = "ubuntu-24.04-template"
+  os_type = "cloud-init"
+  boot = "order=scsi0"
+
+  cores      = 8
+  memory     = 16384
   nameserver = "192.168.1.2"
-  sshkeys = "${local.ssh_public_keys}"
 
-  ipconfig0="gw=192.168.1.1,ip=dhcp"
+  ipconfig0 = "ip=192.168.1.3/24,gw=192.168.1.1"
 
+  scsihw = "virtio-scsi-pci"
   disks {
     ide {
       ide2 {
-        cdrom {
-          iso = "${local.ubuntu_server_iso}"
-        }       
+        cloudinit {
+          storage = "local-lvm"
+        }
       }
     }
     scsi {
       scsi0 {
         disk {
+          size    = "64G"
           storage = "local-lvm"
-          size = "64G"
         }
       }
     }
   }
 
+  vga {
+    type = "serial0"
+  }
+
   network {
     model = "virtio"
-    bridge    = "vmbr0"
+    bridge = "vmbr0"
   }
 }
