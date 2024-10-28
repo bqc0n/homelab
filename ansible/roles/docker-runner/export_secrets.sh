d1171c73913d799494a5f3ee90eb0660cbf13091
bqc0n <b4c0nchan@gmail.com>
2024-10-28 12:20:54 +0900

    fix things & rev proxy

---
+++ ansible/roles/docker-runner/export_secrets.sh
@@ -0,0 +1,6 @@
+if [ -e ./.secrets/influxdb2-admin-token ]; then
+  echo "influxdb2-admin-token already exists. skipping"
+else
+  echo "influxdb2-admin-token does not exist. exporting"
+  op read ""op://Keys/influx-test/credential"" >> ./.secrets/influxdb2-admin-token
+fi
