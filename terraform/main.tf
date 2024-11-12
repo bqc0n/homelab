18e28c97f6862ad7947d8c0129ff6d6d77c5da59
bqc0n <b4c0nchan@gmail.com>
2024-11-12 09:07:31 +0900

    (TT)

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,12 +23,38 @@ locals {
   EOT
 }
 
+resource "proxmox_lxc" "file_server" {
+  vmid = 1001
+  target_node = "pve01"
+  hostname = "FileServer"
+  ostemplate   = "local:vztmpl/ubuntu-24.04-2_amd64.tar.zst"
+  password     = data.sops_file.secrets.data["password.myself"]
+  ssh_public_keys = local.ssh_public_keys
+  start        = true
+  unprivileged = true
+  nameserver = "192.168.1.2"
+
+  memory = 1024
+
+  rootfs {
+    storage = "local-lvm"
+    size    = "8G"
+  }
+
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "192.168.1.5/24"
+    gw     = "192.168.1.1"
+  }
+}
+
 resource "proxmox_lxc" "primary_dns" {
   vmid         = 201
   target_node  = "pve02"
   hostname     = "dnsmasq"
   ostemplate   = "local:vztmpl/ubuntu-24.04-2_amd64.tar.zst"
-  password     = "mou.mai"
+  password     = data.sops_file.secrets.data["password.myself"]
   start        = true
   unprivileged = true
 
