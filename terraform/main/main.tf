77115dd318471c1c9e981fa132d1d2a283cb0b56
bqc0n <b4c0nchan@gmail.com>
2025-01-07 09:44:41 +0900

    fix dns

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -12,7 +12,7 @@ resource "proxmox_lxc" "file_server" {
   password        = data.sops_file.secrets.data["password.myself"]
   ssh_public_keys = local.ssh_public_keys
   unprivileged    = true
-  nameserver      = "192.168.1.2"
+  nameserver      = "192.168.1.1"
 
   onboot = true
   start  = true
@@ -37,3 +37,60 @@ resource "proxmox_lxc" "file_server" {
   }
 }
 
+
+resource "proxmox_vm_qemu" "docker-playground" {
+  vmid = 203
+  name = "docker-playground"
+  vm_state = "running"
+  agent = 0
+
+  target_node      = "pve02"
+  onboot           = true
+  automatic_reboot = true
+
+  cores   = 8
+  sockets = 1
+  memory  = 16384
+  tablet  = false
+  scsihw  = "virtio-scsi-pci"
+
+  clone   = "ubuntu-24.04-template"
+  os_type = "cloud-init"
+  boot    = "order=scsi0"
+
+  nameserver = "8.8.8.8"
+  ipconfig0 = "ip=192.168.1.3/24,gw=192.168.1.1"
+
+  sshkeys = local.ssh_public_keys
+
+  serial {
+    id   = 0
+    type = "socket"
+  }
+
+  disks {
+    scsi {
+      scsi0 {
+        disk {
+          storage    = "local-lvm"
+          size       = "64G"
+          emulatessd = true
+          discard    = true
+        }
+      }
+    }
+    ide {
+      ide2 {
+        cloudinit {
+          storage = "local-lvm"
+        }
+      }
+    }
+  }
+
+  network {
+    # id      = 0
+    model   = "virtio"
+    bridge  = "vmbr0"
+  }
+}
