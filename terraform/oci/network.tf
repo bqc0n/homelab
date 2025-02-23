f9d2e1dc032dd77a557a3f6c1bc1d6ea1db1fecc
bqc0n <b4c0nchan@gmail.com>
2025-02-23 23:12:03 +0900

    feat: OCI IaC Subnet

--- terraform/oci/network.tf
+++ terraform/oci/network.tf
@@ -1,4 +1,4 @@
-resource "oci_core_vcn" "osaka" {
+resource "oci_core_vcn" "osaka_minecraft" {
   compartment_id = oci_identity_compartment.minecraft.id
 
   display_name = "vcn-osaka-minecraft-tf"
@@ -9,4 +9,13 @@ resource "oci_core_vcn" "osaka" {
   freeform_tags = {
     "ManagedBy" = "Terraform"
   }
+}
+
+resource "oci_core_subnet" "osaka_minecraft_public" {
+  cidr_block     = "172.16.0.0/24"
+  compartment_id = oci_identity_compartment.minecraft.id
+  vcn_id         = oci_core_vcn.osaka_minecraft.id
+  ipv6cidr_block = replace(oci_core_vcn.osaka_minecraft.ipv6cidr_blocks[0], "/56", "/64")
+
+  display_name = "subnet-osaka-minecraft-public-tf"
 }
