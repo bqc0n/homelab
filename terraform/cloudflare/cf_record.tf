fac740805dff38c6d1015d0fb49c8348342c27f5
bqc0n <b4c0nchan@gmail.com>
2025-02-27 22:59:01 +0900

    create records with terraform outputs

--- terraform/cloudflare/cf_record.tf
+++ terraform/cloudflare/cf_record.tf
@@ -12,3 +12,19 @@ resource "cloudflare_dns_record" "tunneled_records" {
   proxied = true
   ttl     = 1 # TTL must be "automatic" for proxied records
 }
+
+resource "cloudflare_dns_record" "minecraft_proxy_ipv4" {
+  name    = "minecraft.bqc0n.com"
+  ttl     = 3600
+  type    = "A"
+  zone_id = local.zone_id
+  content = data.terraform_remote_state.cloud.outputs.minecraft-amd-osaka-public-ipv4
+}
+
+resource "cloudflare_dns_record" "minecraft_proxy_ipv6" {
+  name    = "minecraft.bqc0n.com"
+  ttl     = 3600
+  type    = "AAAA"
+  zone_id = local.zone_id
+  content = data.terraform_remote_state.cloud.outputs.minecraft-amd-osaka-ipv6-GUA
+}
--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -21,6 +21,24 @@ data "sops_file" "secrets" {
   source_file = "secrets.enc.yml"
 }
 
+data "terraform_remote_state" "cloud" {
+  backend = "s3"
+  config = {
+    bucket                      = "terraform-backend"
+    key                         = "terraform/cloud/terraform.tfstate"
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+    access_key                  = data.sops_file.secrets.data["r2_access_key"]
+    secret_key                  = data.sops_file.secrets.data["r2_secret_key"]
+    endpoints = { s3 = "https://b10e567bb8146c35a101bec133d4a82f.r2.cloudflarestorage.com" }
+  }
+}
+
 provider "cloudflare" {
   api_token = data.sops_file.secrets.data["cloudflare.api_token"]
 }
