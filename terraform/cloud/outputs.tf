3e30a4eccbbce9d910d3e0c48b0ae2be6143764f
bqc0n <b4c0nchan@gmail.com>
2025-02-27 22:49:36 +0900

    output ipv4/6 of oci instance

--- terraform/cloud/outputs.tf
+++ terraform/cloud/outputs.tf
@@ -1,3 +1,7 @@
-output "minecraft-amd-osaka-public-ip" {
+output "minecraft-amd-osaka-public-ipv4" {
   value = oci_core_instance.minecraft-amd-osaka.public_ip
+}
+
+output "minecraft-amd-osaka-ipv6-GUA" {
+  value = oci_core_instance.minecraft-amd-osaka.create_vnic_details[0].ipv6address_ipv6subnet_cidr_pair_details[0].ipv6address
 }
