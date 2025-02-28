706c69107b3da53a2cd288e7013704a78b41cc35
bqc0n <b4c0nchan@gmail.com>
2025-02-28 09:10:11 +0900

    feat: use remote state

--- terraform/on-premiss/providers.tf
+++ terraform/on-premiss/providers.tf
@@ -1,4 +1,18 @@
 terraform {
+  backend "s3" {
+    bucket                      = "terraform-backend"
+    key                         = "terraform/on-premiss/terraform.tfstate"
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+    access_key                  = ""
+    secret_key                  = ""
+    endpoints = { s3 = "https://b10e567bb8146c35a101bec133d4a82f.r2.cloudflarestorage.com" }
+  }
   required_providers {
     proxmox = {
       source  = "Telmate/proxmox"
