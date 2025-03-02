b2b75f7e73397f30a5413d316659acbdae95b8f3
bqc0n <b4c0nchan@gmail.com>
2025-03-02 15:20:47 +0900

    wip: k8s terraform

---
+++ terraform/kube-cluster/providers.tf
@@ -0,0 +1,28 @@
+terraform {
+  backend "s3" {
+    bucket                      = "terraform-backend"
+    key                         = "terraform/kube-cluster.tfstate"
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+    access_key                  = ""
+    secret_key                  = ""
+    endpoints = { s3 = "https://b10e567bb8146c35a101bec133d4a82f.r2.cloudflarestorage.com" }
+  }
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.0.0-pre2"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
