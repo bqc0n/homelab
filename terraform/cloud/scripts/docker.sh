4bb4e10ed5a800b65c4a617569d390029c69d94a
bqc0n <me@bqc0n.com>
2025-10-12 12:36:39 +0900

    fix: docker.sh destroying fstab

--- terraform/cloud/scripts/docker.sh
+++ terraform/cloud/scripts/docker.sh
@@ -5,7 +5,7 @@ sudo fallocate -l 4G /swapfile2
 sudo chmod 600 /swapfile2
 sudo mkswap /swapfile2
 sudo swapon /swapfile2
-sudo echo "/swapfile2 none swap nw 0 0" | sudo tee /etc/fstab
+sudo echo "/swapfile2 none swap nw 0 0" | sudo tee -a /etc/fstab
 
 # Update pkgs
 sudo dnf check-update
